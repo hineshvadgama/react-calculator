85c536aee00a6a256281af9a473582e627bd3359
hineshvadgama <hinesh.vadgama@outlook.com>
2020-04-29 18:15:06 +0100

    multi digit numbers use separate array elements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Button from '../Button/Button';
 import Screen from '../Screen/Screen';
 import './App.css';
-import {getNumberBeforeOperation, getNumberAfterOperation} from './AppUtils.js';
+import { getNumberBeforeOperation, getNumberAfterOperation } from './AppUtils.js';
+
 
 class App extends React.Component {
 
@@ -62,7 +63,6 @@ class App extends React.Component {
 
     replaceNumberBeforeAndAfterOperationWithOutputValue(arrayWithNumber, positionOfOperation, lengthOfPreviousNumberToRemove, lengthOfAfterNumberToRemove, outputValue) {
 
-        console.log(arrayWithNumber);
         // Remove the number before the operation
         let firstElementToRemove = positionOfOperation - lengthOfPreviousNumberToRemove;
         arrayWithNumber.splice(firstElementToRemove, lengthOfPreviousNumberToRemove);
@@ -74,43 +74,59 @@ class App extends React.Component {
         let secondElementToRemove = positionOfOperation + lengthOfAfterNumberToRemove;
         arrayWithNumber.splice(positionOfOperation + 1, secondElementToRemove);
 
-        // Replace the operation with the output value
-        arrayWithNumber[positionOfOperation] = outputValue.toString();
-        console.log(arrayWithNumber);
+        // Remove the operation from the array
+        arrayWithNumber.splice(positionOfOperation, 1);
+
+        outputValue = outputValue.toString().split("").join(",");
+
+        let outputValueAsArray = outputValue.split(",");
+
+        // Add outputValueAsArray to arrayWithNumber
+        for (let i = 0; i < outputValueAsArray.length; i++) {
+            arrayWithNumber.splice(positionOfOperation, 0, outputValueAsArray[i]);
+            positionOfOperation++;
+        }
+        
 
         this.setState({expression: arrayWithNumber});
     }
 
-    // The problem is I'm editing arrayExpression (this.state.expression) in replaceBeforeAndAfter function whilst iterating through it
+    // The problem WAS no matter how many digits our outputValue is, we add it as a single element in an array
 
-    bidmasCalulator(operation) {
+    bidmasCalulator(operation, noOfOperations) {
         let arrayExpression = this.state.expression;
         let outputValue;
-        for (let i = 0; i < arrayExpression.length; i++) {
-            // This tells us that we've hit a point in the array that has our operation
-            if (arrayExpression[i] === operation) {
-                let numberBeforeOperation = getNumberBeforeOperation(i, arrayExpression);
-                let numberAfterOperation = getNumberAfterOperation(i, arrayExpression);
-                switch (operation) {
-                    case '/':
-                        outputValue = numberBeforeOperation / numberAfterOperation;
+
+        for (let j = 0; j < noOfOperations; j++) {
+
+            for (let i = 0; i < arrayExpression.length; i++) {
+                // This tells us that we've hit a point in the array that has our operation
+                if (arrayExpression[i] === operation) {
+                    let numberBeforeOperation = getNumberBeforeOperation(i, arrayExpression);
+                    let numberAfterOperation = getNumberAfterOperation(i, arrayExpression);
+                    switch (operation) {
+                        case '/':
+                            outputValue = numberBeforeOperation / numberAfterOperation;
+                        break;
+                        case '*':
+                            outputValue = numberBeforeOperation * numberAfterOperation;
+                        break;
+                        case '+':
+                            outputValue = +numberBeforeOperation + +numberAfterOperation;
+                        break;
+                        case '-':
+                            outputValue = numberBeforeOperation - numberAfterOperation;
+                        break;
+                        default:
+                    }
+                    // Replace the number before the operation, the operation, and the number after the operation with outputValue
+                    this.replaceNumberBeforeAndAfterOperationWithOutputValue(
+                        arrayExpression, i, numberBeforeOperation.length, numberAfterOperation.length, outputValue
+                    );
                     break;
-                    case '*':
-                        outputValue = numberBeforeOperation * numberAfterOperation;
-                    break;
-                    case '+':
-                        outputValue = +numberBeforeOperation + +numberAfterOperation;
-                    break;
-                    case '-':
-                        outputValue = numberBeforeOperation - numberAfterOperation;
-                    break;
-                    default:
                 }
-                // Replace the number before the operation, the operation, and the number after the operation with outputValue
-                this.replaceNumberBeforeAndAfterOperationWithOutputValue(
-                    arrayExpression, i, numberBeforeOperation.length, numberAfterOperation.length, outputValue
-                );
             }
+
         }
 
     }
@@ -121,26 +137,26 @@ class App extends React.Component {
 
         const divisionCheck = stringExpression.match(/[/]/g);
         if (divisionCheck !== null) {
-            //const numberOfDivisions = divisionCheck.length;
-            this.bidmasCalulator('/');
+            const numberOfDivisions = divisionCheck.length;
+            this.bidmasCalulator('/', numberOfDivisions);
         }
 
         const multiplicationCheck = stringExpression.match(/[*]/g);
         if (multiplicationCheck !== null) {
-            //const numberOfMultiplications = multiplicationCheck.length;
-            this.bidmasCalulator('*');
+            const numberOfMultiplications = multiplicationCheck.length;
+            this.bidmasCalulator('*', numberOfMultiplications);
         }
 
         const additionCheck = stringExpression.match(/[+]/g);
         if (additionCheck !== null) {
-            //const numberOfAdditions = additionCheck.length;
-            this.bidmasCalulator('+');
+            const numberOfAdditions = additionCheck.length;
+            this.bidmasCalulator('+', numberOfAdditions);
         }
 
         const subtractionCheck = stringExpression.match(/[-]/g);
         if (subtractionCheck !== null) {
-            //const numberOfSubtractions = subtractionCheck.length;
-            this.bidmasCalulator('-');
+            const numberOfSubtractions = subtractionCheck.length;
+            this.bidmasCalulator('-', numberOfSubtractions);
         }
     
         this.setState({screenValue: this.state.expression});
