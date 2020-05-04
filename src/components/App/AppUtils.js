309faf873d3857de99d455460f1ac8ad70c2d40a
hineshvadgama <hinesh.vadgama@outlook.com>
2020-05-04 23:39:32 +0100

    added logic to handle negative numbers correctly

--- src/components/App/AppUtils.js
+++ src/components/App/AppUtils.js
@@ -21,6 +21,8 @@ export function doesCurrentNumberContainDecimal(expressionArray) {
     return decimalStatus;
 }
 
+
+
 export function getNumberBeforeOperation(operationPosition, expressionArray) {
     let counter = operationPosition;
     counter = counter - 1;
@@ -30,27 +32,74 @@ export function getNumberBeforeOperation(operationPosition, expressionArray) {
         numberBeforeOperation.push(expressionArray[counter]);
         counter = counter - 1;
     }
-    numberBeforeOperation = numberBeforeOperation.reverse();
+
+    // In case the number we're trying to get is a negative number
+    let negativeStatus = false;
+
+    if (expressionArray[counter] === '-') {
+
+        if (expressionArray[counter - 1] === '/' || expressionArray[counter - 1] === '*' || expressionArray[counter - 1] === '+'
+        || expressionArray[counter - 1] === '-') {
+
+            negativeStatus = true;
+
+        }
+
+    }
+
+    numberBeforeOperation = numberBeforeOperation.reverse();    
+
+    if (negativeStatus === true) {
+        numberBeforeOperation.unshift("-");
+    }
+
     numberBeforeOperation = numberBeforeOperation.toString();
     numberBeforeOperation = numberBeforeOperation.replace(/,/g, '');
 
     return numberBeforeOperation;
 }
 
+
+
 export function getNumberAfterOperation(operationPosition, expressionArray) {
     let counter = operationPosition;
     counter = counter + 1;
     let numberAfterOperation = [];
+
+    let negativeStatus = false;
+
+    if (expressionArray[counter] === '-') {
+
+        if (expressionArray[counter - 1] === '/' || expressionArray[counter - 1] === '*' || expressionArray[counter - 1] === '+'
+        || expressionArray[counter - 1] === '-') {
+
+            negativeStatus = true;
+
+        }
+
+    }
+
+    if (negativeStatus === true) {
+        counter++;
+    }
     
     while (!isNaN(expressionArray[counter]) || expressionArray[counter] === '.') {
         numberAfterOperation.push(expressionArray[counter]);
         counter = counter + 1;
     }
+
+
+    if (negativeStatus === true) {
+        numberAfterOperation.unshift('-');
+    }
+
     numberAfterOperation = numberAfterOperation.toString();
     numberAfterOperation = numberAfterOperation.replace(/,/g, '');
 
     return numberAfterOperation;
 }
+
+
 
 export function calculatePercentage(num1, num2) {
     return (num2/100) * num1;
